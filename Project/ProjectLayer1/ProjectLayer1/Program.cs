23a41e6e56ae3e6c762e798acdc7ea97583e25c6
urnb <44815860+urnb@users.noreply.github.com>
2020-08-24 18:35:21 +0530

    Add files via upload

---
+++ Project/ProjectLayer1/ProjectLayer1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ProjectLayer2;
+namespace ProjectLayer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patient p = new Patient();
+            Problems pr = new Problems();
+            Treatment t = new Treatment();
+            Patientlog pl = new Patientlog();
+            Staff s = new Staff();
+            Doctor d = new Doctor();
+            Manager m = new Manager();
+            Nurse n = new Nurse();
+            WardBoy w = new WardBoy();
+            Salary sl = new Salary();
+
+            //Console.WriteLine("Hello World!");
+
+        }
+    }
+}
---
+++ Project/ProjectLayer1/ProjectLayer2/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace ProjectLayer2
+{
+    public class Patient
+    {
+        public string name { get; set; }//name compulsory field
+        public string address { get; set; }
+        public int phoneno { get;  set; } // phone no compulsory field
+        //Problem he has mention
+        
+    }
+    public class Problems// allergy are patient specific and not visit specific
+    { 
+        public string allergyname { get; set; }
+        public string describeallergy { get; set; }
+        public string durationofallergy { get; set; }
+
+    }
+    public class Treatment
+    {
+        public string medname;
+        public string desciption;
+        public int frequency = 3;
+    }
+    
+    public class Patientlog
+    {
+        public string date;
+        public string time;
+        public int nooftimesvisited;
+    }
+
+    public class Staff
+    {
+        public string name { get; set; }
+        public int ID { get; set; }
+        public string address { get; set; }
+        public string salary { get; set; }
+        public string DOJ { get; set; }
+        public string Department { get; set; }
+
+    }
+    public class Doctor : Staff
+    { public string education { get; set; }//MBBS and or MD
+    }
+    public class Nurse : Staff
+    {
+    }
+    public class Manager : Staff
+    {
+    }
+    public class WardBoy : Staff
+    {
+    }
+   public class Salary
+    {
+        public void FixedSalary()
+        {
+
+        }
+        public void VariableSalary()
+        {
+
+        }
+    }
+}
